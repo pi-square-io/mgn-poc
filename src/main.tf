25805e3da1694d59d4768aa5bdbdcb621bda2c55
waellatrech <wael.latrech@pi-square.io>
2022-02-01 16:17:27 +0100

    tls fixed & sec grp added

--- src/main.tf
+++ src/main.tf
@@ -11,19 +11,51 @@ resource "tls_private_key" "private_key" {
 }
 
 
+
 resource "aws_key_pair" "generated_key" {
   count      = var.instance_count
-  key_name = "generated_key-${count.index}"
-  public_key = "tls_private_key.private_key.{count.index}.public_key_openssh"
-  tags = merge(var.project_tags, { Name = "generated_key-${count.index}" })
+  key_name   = "generated_key-${count.index}"
+  public_key = tls_private_key.private_key[count.index].public_key_openssh
+  tags       = merge(var.project_tags, { Name = "generated_key-${count.index}" })
 }
 
+resource "local_file" "private_key" {
+    count      = var.instance_count
+    content  = tls_private_key.private_key[count.index].private_key_pem
+    filename = "generated_key-${count.index}.pem"
+}
+resource "aws_security_group" "security_group" {
+  vpc_id = data.aws_vpc.default.id
 
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "security_group"
+  }
+}
 resource "aws_instance" "web" {
   count         = var.instance_count
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "aws_key_pair.generated_key.key_name-${count.index}"
+  key_name      = "generated_key-${count.index}"
 
   user_data     = <<-EOF
   #!/bin/bash
